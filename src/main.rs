29775d1dac8daf461e81c5e62c63d3c9678521f9
yumanuralfath <yumanuralfath2@gmail.com>
2024-12-21 08:30:24 +0700

    feat: update handle port and address

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,26 @@
 #[macro_use]
 extern crate rocket;
 
+use dotenvy::dotenv;
+use std::env;
+
 #[get("/")]
 fn index() -> &'static str {
-    "welcome to the world of pictoria"
+    "Welcome to the world of Pictoria!"
 }
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![index])
+    dotenv().ok();
+
+    let port = env::var("PORT").unwrap_or_else(|_| "8000".to_string());
+    let port: u16 = port.parse().expect("PORT harus berupa angka");
+    let address = env::var("ADDRESS").unwrap_or_else(|_| "127.0.0.1".to_string());
+
+    rocket::custom(
+        rocket::Config::figment()
+            .merge(("port", port))
+            .merge(("address", address)),
+    )
+    .mount("/", routes![index])
 }
