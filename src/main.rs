c8e71de852ac68bc7eaeec291acfb118fda3d17d
yumanuralfath <yumanuralfath2@gmail.com>
2024-12-25 13:24:06 +0700

    feat: add cors support

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,7 @@ mod utils;
 
 use crate::routes::get_routes;
 use dotenvy::dotenv;
+use rocket_cors::{AllowedHeaders, AllowedOrigins};
 use std::env;
 
 #[launch]
@@ -21,6 +22,18 @@ fn rocket() -> _ {
     let port: u16 = port.parse().expect("PORT harus berupa angka");
     let address = env::var("ADDRESS").unwrap_or_else(|_| "127.0.0.1".to_string());
 
+    let allowed_origins =
+        AllowedOrigins::some_exact(&["http://localhost:3000", "https://www.yumana.my.id/"]);
+
+    let cors = rocket_cors::CorsOptions {
+        allowed_origins,
+        allowed_headers: AllowedHeaders::all(),
+        allow_credentials: true,
+        ..Default::default()
+    }
+    .to_cors()
+    .expect("Error konfigurasi CORS");
+
     rocket::custom(
         rocket::Config::figment()
             .merge(("port", port))
@@ -28,4 +41,5 @@ fn rocket() -> _ {
     )
     .mount("/", get_routes())
     .attach(utils::db::attach_db())
+    .attach(cors)
 }
