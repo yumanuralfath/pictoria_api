17ee1814bd803d8b3907bcc2dc74337c91992576
yumanuralfath <yumanuralfath2@gmail.com>
2024-12-29 21:44:04 +0700

    fix: fix wib converter time

--- src/utils/time_converter.rs
+++ src/utils/time_converter.rs
@@ -1,11 +1,11 @@
 use chrono::{FixedOffset, NaiveDateTime, TimeZone};
 
-/// Fungsi untuk mengonversi `NaiveDateTime` ke waktu WIB (UTC+8) dalam format string
+/// Fungsi untuk mengonversi `NaiveDateTime` ke waktu WIB (UTC+7) dalam format string
 pub fn convert_to_wib(naive_datetime: NaiveDateTime) -> String {
-    // Zona waktu UTC+8 (WIB)
-    let wib_offset = FixedOffset::east_opt(8 * 3600).expect("Invalid offset for WIB");
+    // Zona waktu UTC+7 (WIB)
+    let wib_offset = FixedOffset::east_opt(7 * 3600).expect("Invalid offset for WIB");
 
-    // Konversi waktu ke UTC+8 dan ubah menjadi string
+    // Konversi waktu ke UTC+7 dan ubah menjadi string
     wib_offset
         .from_utc_datetime(&naive_datetime)
         .format("%Y-%m-%d %H:%M:%S")
